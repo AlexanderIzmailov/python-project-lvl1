10e30d15bcf914c628a5fc62c08b8dfa43e72112
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-03-26 20:54:14 +0300

    Fix brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from brain_games.games.even import welcome_user
+from brain_games.games.logic import take_name
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    welcome_user()
+    take_name()
 
 
 if __name__ == '__main__':
